c5ef954de1eb75957544bc90b80daeb8043e16d4
codeinja <joseph_thesaint@hotmail.com>
2016-11-14 15:07:03 +0000

    Aesthetic changes

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask import render_template
 import os
 
-app = Flask(__name__)
-app.config.update(TEMPLATES_AUTO_RELOAD=True)
+application = Flask(__name__)
+application.config.update(TEMPLATES_AUTO_RELOAD=True)
     
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
     
 if __name__ == "__main__":
-    app.run(host=os.getenv("IP", "0.0.0.0"), port=os.getenv("PORT", 8080))
+    application.run(host=os.getenv("IP", "0.0.0.0"), port=os.getenv("PORT", 8080))
     
