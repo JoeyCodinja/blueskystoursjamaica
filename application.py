8d741c76f466f1b3f9a50a91f02154052b4afabe
Danuel Williams <joseph_thesaint@hotmail.com>
2016-11-13 20:21:07 -0500

    changed app.py to application.py

---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+import os
+
+application = Flask(__name__)
+application.config.update(TEMPLATES_AUTO_RELOAD=True)
+
+@application.route('/')
+@application.route('/index')
+def index():
+    return render_template('index.html')
+    
+if __name__ == "__main__":
+    application.run(host=os.getenv("IP", "0.0.0.0"), port=os.getenv("PORT", 5000))
+    
